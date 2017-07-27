d6e44e5d38bcd31b54c5f23266d3c0d6e9b83b5f
Shashank Kaul <kaulshashank96@gmail.com>
2017-07-27 18:16:50 +0530

    Complete Overhaul
    Removed old AngularJS API call and restructured the application using ExpressJS and EJS views.
    
    - ExpressJS and EJS added
    - Header, footer, and index views created.
    - Displays menu on accessing main page.
    - Structured basic 4 W questions to access travel schedule.

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-var app = angular.module('myApp', []);
-var api_key = "qg440drq";
-app.controller('myController', function($scope, $http) {
-    $scope.orig = "";
-    $scope.dest = "";
-    $scope.mm = "";
-    $scope.dd = "";
-    $scope.trains= [];
-    $scope.Submit = function() {
-      if($scope.mode == "TRAIN") {
-        $http.get('http://api.railwayapi.com/between/source/'+$scope.orig+'/dest/'+$scope.dest+'/date/'+$scope.dd+'-'+$scope.mm+'/apikey/'+api_key+'/')
-        .success(function(data, status) {     
-          $scope.trains = data;
-          console.log(JSON.stringify(data));
-        })
-        .error(function(data, status) {
-          console.log("some error");
-        });
-      }
-    }
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public')); //Allows us to use "public" folder to store stylesheets and images.
+
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
